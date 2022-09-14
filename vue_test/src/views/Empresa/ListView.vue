a001c3a6651964b1d3f84ac7f2d8ffa1d34ec58c
Gabriel Fidelis Pereira <jpinformaticafoz@gmail.com>
2022-09-14 00:57:11 -0300

    'paginate'

--- vue_test/src/views/Empresa/ListView.vue
+++ vue_test/src/views/Empresa/ListView.vue
@@ -1,58 +1,68 @@
+
 <template>
-    <div>
+    <b-container >
+        <div>
         <h1>Listagem de Empresas</h1>
         <div class="row mt-5">
             <div class="col-6">
                 <b-button v-b-modal.modal-1>Inserir Usuario</b-button>
 
                 <b-modal id="modal-1" title="Adicionar Empresa">
-                    <div class="row">
-                        <div class="col-6">
+                    <b-row>
+                        <b-col>
                             <b-input v-model="NOME" placeholder="Nome"></b-input>
-                        </div>
-                        <div class="col-6">
+                        </b-col>
+                        <b-col>
                             <b-input v-model="CNPJ" placeholder="Cnpj"></b-input>
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-12">
-                            <b-button @click="addEmpresa" block variant="outline-success" >Adicionar</b-button>
-                        </div>
-                    </div>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col class="text-center">
+                            <b-button @click="addEmpresa" pill variant="outline-success" class="raise" >Adicionar</b-button>
+                        </b-col>
+                    </b-row>
                 </b-modal>
             </div>
             <div class="col-6">
-                <b-button variant="outline-primary">Voltar</b-button>
+                <router-link to="/"><b-button variant="outline-primary">Voltar</b-button></router-link> 
             </div>
         </div>
-        <table class="table mt-5 table-responsive-sm">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">NOME</th>
-                <th scope="col">CNPJ</th>
-                <th scope="col">EDITAR</th>
-                <th scope="col">EXCLUIR</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="Empresa in listagem" :key="Empresa.ID">
-                <th scope="row">{{Empresa.ID}}</th>
-                <td>{{Empresa.NOME}}</td>
-                <td>{{Empresa.CNPJ}}</td>
-                <td><b-button variant="outline-success">Editar</b-button></td>
-                <td><b-button variant="outline-danger">Excluir</b-button></td>
-              </tr>
-            </tbody>
-          </table>
-    </div>
+        
+            <b-row>
+                <b-col>
+                    <table class="table mt-5 table-responsive-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">NOME</th>
+                            <th scope="col">CNPJ</th>
+                            <th scope="col">EDITAR</th>
+                            <th scope="col">EXCLUIR</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="Empresa in listagem" :key="Empresa.ID">
+                            <th scope="row">{{Empresa.ID}}</th>
+                            <td>{{Empresa.NOME}}</td>
+                            <td>{{Empresa.CNPJ}}</td>
+                            <td><b-button variant="outline-success">Editar</b-button></td>
+                            <td><b-button variant="outline-danger">Excluir</b-button></td>
+                          </tr>
+                          <b-button @click="previusPage" variant="outline-sucess">Pagina Anterior</b-button> 
+                          <b-button @click="nextPage" variant="outline-sucess">Proxima Pagina</b-button> 
+                        </tbody>
+                      </table>
+                </b-col>
+            </b-row>
+        </div>
+    </b-container>
 </template>
 
 <script>
 
 
 
-
+/* eslint-disable */
 export default {
     
     data(){
@@ -74,14 +84,26 @@ export default {
             this.$http.post('/empresas', data).then((response)=>{
                 console.log(response);
             })
-            this.listagem.push(data);
             this.NOME = '';
             this.CNPJ = '';
         },
-         async getEmpresas(page = 1){
+         async getEmpresas(page){
+           if(!this.page >= 1){
+                this.page = 1;
+           }
+           this.total = this.page + 1
            const res =  await this.$http.get('/empresas?page=' + this.page + '&total=' + this.total);
            this.listagem = res.data.data
            console.log(this.listagem);
+        },
+
+        nextPage(){
+            this.page += 1
+            this.getEmpresas(this.page)
+        },
+        previusPage(){
+            this.page -= 1
+            this.getEmpresas(this.page)
         }
     },
     async created(){
@@ -91,4 +113,12 @@ export default {
 </script>
 
 <style>
+    .body-table{
+        width: 100%;
+    }
+    .raise:hover,
+    .raise:focus {
+        box-shadow: 0 0.5em 0.5em -0.4em var(--hover);
+        transform: translateY(-0.25em);
+    }
 </style>
